20d30d647673f83164aa50046df3010e128b4f19
shahbaz1302 <khanmohdshahbaz70@gmail.com>
2026-08-19 19:58:29 +0530

    resume link updated

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -71,7 +71,7 @@ const AboutPage = () => {
           <a href="https://www.linkedin.com/in/mohd-shahbaz-khannn" target="_blank" rel="noopener noreferrer"><RiLinkedinLine /></a>
           <a href="https://github.com/shahbaz1302" target="_blank" rel="noopener noreferrer"><FiGithub /></a>
         </div>
-        <button className="btn contact-btn" ref={btnRef}><a href="https://drive.google.com/file/d/1HcJpaZupD6atTTU99cmsiXBpCWlM84bo/view?usp=drive_link"
+        <button className="btn contact-btn" ref={btnRef}><a href="https://drive.google.com/file/d/1lEVgO3pvfwev8d10QKUH7cyzEBQmtTD0/view?usp=drive_link"
           target="_blank">My Resume</a></button>
       </div>
       <div className="what-section">
